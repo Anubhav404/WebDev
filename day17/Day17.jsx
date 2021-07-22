fa04a337db80d4d00d620f0d9dca9f99b25e26c3
Anubhav404 <84443881+Anubhav404@users.noreply.github.com>
2021-07-22 23:30:05 +0530

    Add files via upload

---
+++ day17/Day17.jsx
@@ -0,0 +1,53 @@
+import React from "react"
+
+
+function Day17() {
+    return (
+        <>
+
+            <div className="main-heading">
+                <h1> Instruction </h1>
+                <ol>
+                    <li>build a container</li>
+                    <li>create a seperate function and use it as a component</li>
+                    <li>pass props "calory and food" and call it to main component</li>
+                </ol>
+            </div>
+            <h1>Calories Read Only</h1>
+
+        <div className="scroll-bar">
+            <div className="scroll">
+                <div className="box">
+                    <h2>Pizza</h2>
+                    <h3>You Have consumed 200cals</h3>
+                </div>
+                <div className="box">
+                    <h2>Pizza</h2>
+                    <h3>You Have consumed 200cals</h3>
+                </div>
+
+                <div className="box">
+                    <h2>Pizza</h2>
+                    <h3>You Have consumed 200cals</h3>
+                </div>
+                <div className="box">
+                    <h2>Pizza</h2>
+                    <h3>You Have consumed 200cals</h3>
+                </div>
+                <div className="box">
+                    <h2>Pizza</h2>
+                    <h3>You Have consumed 200cals</h3>
+                </div>
+                <div className="box">
+                    <h2>Pizza</h2>
+                    <h3>You Have consumed 200cals</h3>
+                </div>
+
+        
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Day17;
---
+++ day17/index.js
@@ -0,0 +1,21 @@
+
+import React from "react"
+
+import ReactDom from "react-dom"
+
+import './index.css'
+
+
+
+
+// import Apps from "./Apps"
+
+import Day17 from "./Day17"
+
+
+
+
+
+
+ReactDom.render( <Day17 /> , document.getElementById("root")
+)
