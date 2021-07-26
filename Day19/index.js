ff621bd4a6f4560cf7ad1b590725cfc6607d8758
Anubhav404 <84443881+Anubhav404@users.noreply.github.com>
2021-07-26 19:31:33 +0530

    Add files via upload

---
+++ Day19/index.js
@@ -0,0 +1,24 @@
+
+import React from "react"
+
+import ReactDom from "react-dom"
+
+// import './card.css'
+
+// import Apps from "./Apps"
+
+// // import Day17 from "./Day17"
+
+// // import Day18  from "./Day18"
+// // import "./chess.css"
+import "./Increment.css"
+
+
+// import Slot from "./Slot"
+
+import Increment from "./Increment"
+
+// import Whatsapp from "./Whatsapp"
+
+
+ReactDom.render(<Increment /> , document.getElementById("root"))
